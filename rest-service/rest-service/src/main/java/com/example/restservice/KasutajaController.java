04899c54039479deb8100bebe09559ff4fd0f5ed
Artur Kreegipuu <artur@kreegipuu.net>
2022-06-20 13:39:05 +0300

    KasutajaService

--- rest-service/rest-service/src/main/java/com/example/restservice/KasutajaController.java
+++ rest-service/rest-service/src/main/java/com/example/restservice/KasutajaController.java
@@ -1,5 +1,6 @@
 package com.example.restservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,17 @@ import java.util.List;
 
 @RestController
 public class KasutajaController {
+
+    private final KasutajaService kasutajaService;
+
+    @Autowired
+    public KasutajaController(KasutajaService kasutajaService) {
+        this.kasutajaService = kasutajaService;
+    }
+
     @GetMapping("/kasutaja")
     public List<Kasutaja> getKasutaja(){
-        return List.of(new Kasutaja("Artur", 21, LocalDate.of(2001, Month.APRIL, 20), "artur@kreegipuu.net"));
+        return kasutajaService.getKasutaja();
     }
 
 
---
+++ rest-service/rest-service/src/main/java/com/example/restservice/KasutajaService.java
@@ -0,0 +1,17 @@
+package com.example.restservice;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class KasutajaService {
+    private final AtomicLong counter = new AtomicLong();
+    public List<Kasutaja> getKasutaja(){
+        return List.of(new Kasutaja(counter.incrementAndGet(), "Artur", 21, LocalDate.of(2001, Month.APRIL, 20), "artur@kreegipuu.net"));
+    }
+}
