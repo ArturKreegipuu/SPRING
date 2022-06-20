4b16241f8eb5d44bb961ceb8abd616e529adce3d
Artur Kreegipuu <92913834+ArturKreegipuu@users.noreply.github.com>
2022-06-20 11:37:14 +0300

    Add files via upload
    Building a RESTful Web Service õpetus + ise tehtud tervitusklass

---
+++ rest-service/rest-service/src/main/java/com/example/restservice/Tervitus.java
@@ -0,0 +1,13 @@
+package com.example.restservice;
+
+public class Tervitus {
+    private final String content;
+
+    public Tervitus(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
