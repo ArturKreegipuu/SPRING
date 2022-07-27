0770bf21d19d0395af6815770fd1bd0cba1099ee
Artur Kreegipuu <artur@kreegipuu.net>
2022-07-27 16:42:14 +0300

    global .gitignore?

--- rest-service/rest-service/src/main/java/com/example/restservice/Greeting.java
+++ rest-service/rest-service/src/main/java/com/example/restservice/Greeting.java
@@ -1,17 +1,19 @@
 package com.example.restservice;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 
+@AllArgsConstructor
 @Getter
 public class Greeting {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+   /* public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
-    }
+    }*/
 }
 // https://spring.io/guides/gs/rest-service/#scratch
